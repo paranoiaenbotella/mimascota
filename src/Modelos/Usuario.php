1218a313bfb54ba27b15f4c9b9c57de1c5cc98c4
Ilya Bakhlin <github@ilyabakhlin.com>
2020-04-06 17:33:08 +0200

    Fixing the insertarUsuario method in the Usuario model class.

--- src/Modelos/Usuario.php
+++ src/Modelos/Usuario.php
@@ -4,9 +4,11 @@ require_once(dirname(__DIR__) . "/Modelo.php");
 
 class Usuario extends Modelo
 {
-    public function insertarUsuario($nombre, $apellidos, $email,$contrasena, $imagen,  )
+    public function insertarUsuario($nombre, $apellidos, $email, $contrasena, $imagen, $idRol)
     {
-        return $this->ejecutarConsulta("INSERT INTO `usuarios` (nombre, apellidos, email, contrasena, imagen) VALUE ('$nombre', '$apellidos', '$email', '$contrasena', '$imagen', )" );
+        return $this->ejecutarConsulta(
+            "INSERT INTO `usuarios` (`nombre`, `apellidos`, `email`, `contrasena`, `imagen`, `id_roles`) VALUE ('$nombre', '$apellidos', '$email', '$contrasena', '$imagen', $idRol)"
+        );
     }
 
     public function leerPorId($id)
