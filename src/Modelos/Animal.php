2aca1fcc73d15aaa74a3ed14bb9baf5206eb921c
Ilya Bakhlin <github@ilyabakhlin.com>
2020-04-06 18:35:09 +0200

    Fixing the query registering for inserting the animal in the database.

--- src/Modelos/Animal.php
+++ src/Modelos/Animal.php
@@ -4,9 +4,11 @@ require_once(dirname(__DIR__) . "/Modelo.php");
 
 class Animal extends Modelo
 {
-    public function insertarAnimal($nombre)
+    public function insertarAnimal($idAnimalesTipo, $idUsuario, $nombre)
     {
-        return $this->ejecutarConsulta("INSERT INTO `animales` (`nombre`) VALUE ('$nombre')");
+        return $this->ejecutarConsulta(
+            "INSERT INTO `animales` (`id_animales_tipos`, `id_usuarios`, `nombre`) VALUE ($idAnimalesTipo, $idUsuario, '$nombre')"
+        );
     }
 
     public function leerPorId($id)
