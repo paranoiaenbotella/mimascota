ba31c7fd7e9056a8b73c015d5bd262631484d27c
Ilya Bakhlin <github@ilyabakhlin.com>
2020-04-06 18:10:44 +0200

    Fixing the address insertion and adding some comments.

--- src/Modelos/Direccion.php
+++ src/Modelos/Direccion.php
@@ -1,13 +1,19 @@
 <?php
 
-//la clase Dirección realiza las consultas relacionadas con la dirección del usuario
 require_once(dirname(__DIR__) . "/Modelo.php");
 
-class Direccion extends Modelo {
-
-	public function insertarDireccion($pais, $ciudad, $codigo_postal, $calle, $imagen1, $imagen2, $imagen3, $imagen4)
-    {
-    	return $this->ejecutarConsulta("INSERT INTO 'direcciones' ('pais', 'ciudad', 'codigo_postal', 'calle', 'imagen1', 'imagen2', 'imagen3', 'imagen4', ) VALUE ('$pais', '$ciudad', '$codigo_postal', '$calle', '$imagen1', '$imagen2', '$imagen3', '$imagen4', )");
+/**
+ * La clase Dirección realiza las consultas relacionadas con la dirección del usuario.
+ */
+class Direccion extends Modelo
+{
+    /**
+     * Este método inserta una dirección, vinculada a un usuario, a la base de datos.
+     */
+    public function insertarDireccion($pais, $ciudad, $codigo_postal, $calle, $imagen1, $imagen2, $imagen3, $imagen4, $idUsuario) {
+        return $this->ejecutarConsulta(
+            "INSERT INTO 'direcciones' (`id_usuarios`, `pais`, `ciudad`, `codigo_postal`, `calle`, `imagen1`, `imagen2`, `imagen3`, `imagen4`) VALUE ($idUsuario, '$pais', '$ciudad', '$codigo_postal', '$calle', '$imagen1', '$imagen2', '$imagen3', '$imagen4')"
+        );
     }
 
     public function leerPorIdUsuario($id_usuarios)
