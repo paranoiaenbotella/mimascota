e2b6c31f44986c169fb814ce1f4b7749dc97a65d
Ilya Bakhlin <github@ilyabakhlin.com>
2020-04-07 17:27:04 +0200

    Fixing the AnimalTipo class.

--- src/Modelos/AnimalTipo.php
+++ src/Modelos/AnimalTipo.php
@@ -1,16 +1,15 @@
 <?php
 
 require_once(dirname(__DIR__) . "/Modelo.php");
+
 /**
-*La clase realiza las operaciones en la 
-*base de datos relacionadas con tipos de animales
-*/
-
-class AnimalTipo extends Modelo {
-
-	public function insertarTipoAnimal ($nombre){
-		return $this->ejecutarConsulta(
-            "INSERT INTO `animales_tipo` (`nombre`) VALUE ('$nombre')");
-        );
-	}
+ * La clase realiza las operaciones en la
+ * base de datos relacionadas con tipos de animales
+ */
+class AnimalTipo extends Modelo
+{
+    public function insertarTipoAnimal($nombre)
+    {
+        return $this->ejecutarConsulta("INSERT INTO `animales_tipos` (`nombre`) VALUE ('$nombre')");
+    }
 }
