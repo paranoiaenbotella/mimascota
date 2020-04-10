a7422fe6c26b5c9c40b4169291dfa72320237427
Ilya Bakhlin <github@ilyabakhlin.com>
2020-04-10 17:13:53 +0200

    Reformatting the ejecutarConsulta method.

--- src/Modelo.php
+++ src/Modelo.php
@@ -23,16 +23,16 @@ class Modelo
     }
 
     protected function ejecutarConsulta($consulta)
-    {   
+    {
         $tabla = [];
         $resultado = $this->conexion->query($consulta);
         if (is_bool($resultado)) {
             return $resultado;
         } else {
-                while($fila = $resultado->fetch_assoc()){
-                    $tabla[] = $fila;
-                }
-                    return $tabla;
+            while ($fila = $resultado->fetch_assoc()) {
+                $tabla[] = $fila;
+            }
+            return $tabla;
         }
     }
 
