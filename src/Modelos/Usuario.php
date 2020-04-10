704f7f8ca66cdb48a644ae5f99cab3a3cc771a0b
Ilya Bakhlin <github@ilyabakhlin.com>
2020-04-10 19:46:03 +0200

    Adding some comments to the Usuario model.

--- src/Modelos/Usuario.php
+++ src/Modelos/Usuario.php
@@ -4,28 +4,47 @@ require_once(dirname(__DIR__) . "/Modelo.php");
 
 class Usuario extends Modelo
 {
-    public function insertarUsuario($nombre, $apellidos, $email, $contrasena, $imagen, $idRol)
+    public function insertarUsuario(
+        $nombre,
+        $apellidos,
+        $email,
+        $contrasena,
+        $imagen,
+        $idRol
+    ) // TODO: Este métido necesita ser refactorizado o eliminado.
     {
         return $this->ejecutarConsulta(
             "INSERT INTO `usuarios` (`nombre`, `apellidos`, `email`, `contrasena`, `imagen`, `id_roles`) VALUE ('$nombre', '$apellidos', '$email', '$contrasena', '$imagen', $idRol)"
         );
     }
 
-    public function insertarCuidador($nobmre, $apellidos, $email, $contrasena, $imagen)
+    public function insertarCuidador(
+        $nobmre,
+        $apellidos,
+        $email,
+        $contrasena,
+        $imagen
+    ) // TODO: Este métido necesita ser refactorizado o eliminado.
     {
         $cuidadorConsulta = "SELECT `id` FROM `roles` WHERE `nombre` = 'Cuidador'";
         $cuidador = $this->ejecutarConsulta($cuidadorConsulta);
         return $this->insertarUsuario($nobmre, $apellidos, $email, $contrasena, $imagen, $cuidador["id"]);
     }
 
-    public function insertarPropietario($nombre, $apellidos, $email, $contrasena, $imagen)
+    public function insertarPropietario(
+        $nombre,
+        $apellidos,
+        $email,
+        $contrasena,
+        $imagen
+    ) // TODO: Este métido necesita ser refactorizado o eliminado.
     {
         $propietarioConsulta = "SELECT `id` FROM `roles` WHERE `nombre`= 'Propietario'";
         $propietario = $this->ejecutarConsulta($propietarioConsulta);
         return $this->insertarUsuario($nombre, $apellidos, $email, $contrasena, $imagen, $propietario["id"]);
     }
 
-    public function leerPorId($id)
+    public function leerPorId($id) // TODO: Este método necesita ser refactorizado o eliminado.
     {
         return $this->ejecutarConsulta("SELECT * FROM `usuarios` WHERE `id` = $id");
     }
