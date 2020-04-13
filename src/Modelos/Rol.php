fec470c0a15dea3c2580a2e3a7e94500151b892c
Ilya Bakhlin <github@ilyabakhlin.com>
2020-04-13 16:30:08 +0200

    Improving the Rol model class.

--- src/Modelos/Rol.php
+++ src/Modelos/Rol.php
@@ -8,48 +8,65 @@ require_once(dirname(__DIR__) . "/Modelo.php");
  */
 class Rol extends Modelo
 {
+    private $id;
+
     private $nombre;
 
-/**
- * Métodos para definir y obtener datos
- */
+    /**
+     * Métodos para definir y obtener datos
+     */
     public function definirNombre($nombre)
     {
-        $nombreValido = filter_var($nombre, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        if ($nombreValido === false) {
-            throw new Exception("El nombre introducido no es valido");
-        
-        }   else {
-            $this->nombre = $nombreValido;
+        if (empty($nombre)) {
+            throw new Exception("El nombre introducido no puede ser vacío.");
+        } else {
+            $this->nombre = filter_var($nombre, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         }
+        return $this;
     }
 
-    public function obtenerNombre(){
-        if (is_null($this->nombre)){
-        throw new Exception("El nombre del rol no esta definido");
-        
-        }   else {
-            return $this->nombre;
-        }
-    }
-/**
- * Métodos que realizan las operaciones requeridas 
- * por la aplicación en la BD.
- */
-    public function insertarRol($nombre)
+    public function insertar()
     {
-        return $this->ejecutarConsulta("INSERT INTO `roles` (`nombre`) VALUE ('$nombre')");
+        $consulta = $this->conexion->prepare("INSERT INTO `roles` (`nombre`) VALUE (?)");
+        $consulta->bind_param("s", $this->nombre);
+        $result = $consulta->execute();
+        $consulta->close();
+        return $result;
     }
-
 
     public function leerPorId($id)
     {
         return $this->ejecutarConsulta("SELECT * FROM `roles` WHERE `id` = $id");
     }
 
+    public function listarRoles()
+    {
+        $roles = [];
+        $resultado = $this->conexion->query("SELECT * FROM `roles` ORDER BY `id`");
+        while ($fila = $resultado->fetch_assoc()) {
+            $rol = new Rol();
+            $rol->id = $fila["id"];
+            $rol->nombre = $fila["nombre"];
+            array_push($roles, $rol);
+        }
+        return $roles;
+    }
 
-	public function listarRoles()
-	{
-		return $this->ejecutarConsulta("SELECT * FROM `roles` ORDER BY `id`");
-	}
+    public function obtenerId()
+    {
+        if (is_null($this->id)) {
+            throw new Exception("El identificador del rol no está definido.");
+        } else {
+            return $this->id;
+        }
+    }
+
+    public function obtenerNombre()
+    {
+        if (is_null($this->nombre)) {
+            throw new Exception("El nombre del rol no está definido.");
+        } else {
+            return $this->nombre;
+        }
+    }
 }
