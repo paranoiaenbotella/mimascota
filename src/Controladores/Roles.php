2298fbc986557861d07aa43ba76a011ddae0f249
Ilya Bakhlin <github@ilyabakhlin.com>
2020-04-22 18:17:48 +0200

    Reformatting the Roles controller.

--- src/Controladores/Roles.php
+++ src/Controladores/Roles.php
@@ -1,26 +1,35 @@
 <?php
 
-
 require_once(dirname(__DIR__) . "/Modelos/Rol.php");
 
 /**
- * Mediante esta clase se controla las operaciones sobre la tabla 'roles' 
+ * Mediante esta clase se controla las operaciones sobre la tabla 'roles'
  * y muestra por pantalla los resultados utilizando las vista correspondiente
  * para cada operación
  */
 class Roles
 {
-/**
- * Método que muestra el formulario de ingreso 
- */
+    /**
+     * Método que muestra el formulario de ingreso
+     */
     public function getCrear()
     {
         require_once(dirname(__DIR__) . "/Vistas/Roles/Crear.php");
     }
 
-/**
- * Mediante este método se controla la inserción del tipo de rol en la BD
- */
+    /**
+     * Mediante este método se muestra por pantalla los registros de tipos de roles
+     */
+    public function getListar()
+    {
+        $rol = new Rol();
+        $nombreRol = $rol->listarRoles();
+        require_once(dirname(__DIR__) . "/Vistas/Roles/Listar.php");
+    }
+
+    /**
+     * Mediante este método se controla la inserción del tipo de rol en la BD
+     */
     public function postCrear()
     {
         if (empty($_POST["nombre"])) {
@@ -33,14 +42,5 @@ class Roles
                 echo("No se ha podido crear el rol.");
             }
         }
-    }
-/**
- * Mediante este método se muestra por pantalla los registros de tipos de roles
- */
-    public function getListar()
-    {   
-       $rol = new Rol();
-       $nombreRol = $rol->listarRoles();
-       require_once(dirname(__DIR__) . "/Vistas/Roles/Listar.php"); 
     }
 }
