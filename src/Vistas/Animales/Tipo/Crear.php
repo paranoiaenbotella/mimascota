24f7c19af0eb7ac3136b9d51287783d282d87fcb
Ilya Bakhlin <github@ilyabakhlin.com>
2020-05-10 19:24:32 +0200

    Reformatting the view.

--- src/Vistas/Animales/Tipo/Crear.php
+++ src/Vistas/Animales/Tipo/Crear.php
@@ -15,23 +15,23 @@
                     <form method="POST">
                         <div class="form-group my-0">
                             <label for="nombre" class="mb-0">Tipo de animal:</label>
-                            <input class ="form-control form-control-sm" id="nombre" name="nombre"  type="text">
-                            <?php if (Sesion::existeError("nombreTipoAnimal")):?>
-                            <div>
-                                <p class="error"><?php echo(Sesion::obtenerError("nombreTipoAnimal"));?></p>
-                            </div>
-                            <?php elseif(Sesion::exiteAcierto("succes")): ?>
-                            <div>
-                                <p class="succes"><?php echo(Sesion::obtenerAcierto("succes"));?></p>
-                            </div>
-                            <?php else:?>
+                            <input class="form-control form-control-sm" id="nombre" name="nombre" type="text">
+                            <?php if (Sesion::existeError("nombreTipoAnimal")): ?>
+                                <div>
+                                    <p class="error"><?php echo(Sesion::obtenerError("nombreTipoAnimal")); ?></p>
+                                </div>
+                            <?php elseif (Sesion::exiteAcierto("succes")): ?>
+                                <div>
+                                    <p class="succes"><?php echo(Sesion::obtenerAcierto("succes")); ?></p>
+                                </div>
                             <?php endif; ?>
                             <div class="form-group my-2">
-                                <input class ="btn btn-block btn-success"type="submit" value="Crear">
+                                <input class="btn btn-block btn-success" type="submit" value="Crear">
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
-            </section>          
-        </body>
-    </html>
+            </div>
+        </section>
+    </body>
+</html>
