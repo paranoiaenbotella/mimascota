1b21dd653248b199ad5acbc7e1a7db6f9f1aa2b7
paranoiaenbotella <mikahe@tutanota.de>
2020-05-14 16:25:03 +0200

    Data base

---
+++ src/Modelos/Anuncio.php
@@ -0,0 +1,217 @@
+<?php
+
+require_once(dirname(__DIR__)."Modelo.php");
+
+/**
+ * Operaciones contra la tabla anuncios
+ */
+
+class Anuncio extends Modelo
+{
+	private $id;
+
+	private $usuario;
+
+	private $tarifa;
+
+	private $fecha;
+
+	private $imagen1;
+
+    private $imagen2;
+
+    private $imagen3;
+
+    private $imagen4;
+
+
+     /**
+     * Se define y se fuerza que el tipo
+     * de la $id sea Int
+     */
+    private function definirId($id)
+    {
+        $this->id = (int)$id;
+    }
+
+/**
+ * Método para obtener el id
+ */
+    public function obtenerId()
+    {
+        if (is_null($this->id)) {
+            throw new Exception("El identificador del anuncio no está definido.");
+        } else {
+            return $this->id;
+        }
+    }
+
+    /**
+     * Se crea al usuario propietario del anuncio
+     */
+public function crearUsuario($usuario){
+
+    if ($usuario instanceof Usuario){
+        $this->usuario = $usuario->obtenerId();
+    } else {
+        throw new Exception("El parámetro facilitado no es una instancia de la clase Usuario.");
+
+    }
+}
+
+    /**
+     * Se define y se fuerza que el tipo
+     * de la $usuario sea Int
+     */
+    public function definirUsuario($usuario){
+        $this->usuario = (int)$usuario;
+    }
+
+/**
+ * Método para obtener el usuario
+ */
+    public function obtenerUsuario()
+    {
+        if (is_null($this->usuario)) {
+            throw new Exception("El usuario no está definido");
+        } else {
+            return $this->usuario;
+        }
+    } 
+
+
+
+
+
+/**
+ * Método para definir la fecha de creación
+ */
+
+public function definirFecha($fecha){
+	
+	if (is_null($fecha)){
+		Sesion::definirError("No hay una fecha creada.", "fecha");
+	} else{
+		$this->fecha = date_format($fecha, "d/m/y");
+	}
+
+	return this;
+}
+
+/**
+ * Método para obtener la fecha de creación
+ */
+public function obtenerFecha()
+{
+	if(is_null($this->fecha)){
+		throw new Exception("La fecha no está definida");	
+	} else {
+
+
+
+		return $this->fecha;
+	}
+}
+
+/**
+ * Método para definir la imagen 1
+ */
+    public function definirImagen1($imagen1)
+    {
+        $imagen1Valida = filter_var($imagen1, FILTER_SANITIZE_URL);
+        if ($imagen1Valida === false) {
+            throw new Exception("La ruta de la imagen es incorrecta");
+        }   else {
+            $this->imagen1 = $imagen1Valida;
+        }
+    }
+
+/**
+ * Método para definir la imagen 2
+ */
+    public function definirImagen2($imagen2)
+    {
+        $imagen2Valida = filter_var($imagen2, FILTER_SANITIZE_URL);
+        if ($imagen2Valida === false) {
+            throw new Exception("La ruta de la imagen es incorrecta");
+        }   else {
+            $this->iamgen2 = $imagen2Valida;
+        }
+    }
+
+/**
+ * Método para definir la imagen 3
+ */
+    public function definirImagen3($imagen3)
+    {
+        $imagen3Valida = filter_var($imagen3, FILTER_SANITIZE_URL);
+        if ($imagen3Valida === false) {
+            throw new Exception("La ruta de la imagen es incorrecta");
+        }   else {
+            $this->imagen3 = $imagen3Valida;
+        }
+    }
+
+/**
+ * Método para definir la imagen 4
+ */
+    public function definirImagen4($imagen4)
+    {
+        $imagen4Valida = filter_var($imagen4, FILTER_SANITIZE_URL);
+        if ($imagen4Valida === false) {
+            throw new Exception("La ruta de la imagen es incorrecta");
+        }   else {
+            $this->iamgen4 = $imagen4Valida;
+        }
+    } 
+
+ /**
+ * Método para obtener la imagen 1
+ */
+    public function obtenerImagen1()
+    {
+        if (is_null($this->imagen1)){
+        throw new Exception("Ruta de la imagen no definida");
+        }   else {
+            return $this->imagen1;
+        }
+    }
+
+/**
+ * Método para obtener la imagen 2
+ */
+    public function obtenerImagen2()
+    {
+        if (is_null($this->imagen2)){
+        throw new Exception("Ruta de la imagen no definida");
+        }   else {
+            return $this->imagen2;
+        }
+    }
+
+/**
+ * Método para obtener la imagen 3
+ */
+    public function obtenerImagen3()
+    {
+        if (is_null($this->imagen3)){
+        throw new Exception("Ruta de la imagen no definida");
+        }   else {
+            return $this->imagen3;
+        }
+    }
+
+/**
+ * Método para obtener la imagen 4
+ */
+    public function obtenerImagen4()
+    {
+        if (is_null($this->imagen4)) {
+            throw new Exception("Ruta de la imagen no definida");
+        } else {
+            return $this->imagen4;
+        }
+    }
+
+
+}
---
+++ src/Modelos/Servicio.php
@@ -0,0 +1,253 @@
+<?php
+
+require_once(dirname(__DIR__) . "/Modelo.php");
+
+/**
+ * La clase contiene las operaciones contra la BD y los métodos
+ * que definen, obtienen y validan los datos.
+ */
+ class Tarifa extends Modelo
+ {
+ 	private $id;
+ 	private $usuario;
+ 	private $anuncio;
+ 	private $nombre;
+    private $descripcion;
+ 	private $precio;
+
+
+/**
+ * Se define el id
+ */
+    private function definirId($id)
+    {
+        $this->id = (int)$id;
+    }
+
+/**
+ * Método para obtener el id
+ */
+    public function obtenerId()
+    {
+        if (is_null($this->id)) {
+            throw new Exception("El identificador del tarifa no está definido.");
+        } else {
+            return $this->id;
+        }
+    } 
+
+/**
+ * Crear usuario para el registro
+ */
+	public function crearUsuario($usuario)
+    {
+        if ($usuario instanceof Rol) {
+            $this->usuario = $usuario->obtenerId();
+        } else {
+            throw new Exception("El parámetro facilitado no es una instancia de la clase Usuario");
+        }
+        return $this;
+    }
+
+/**
+ * Definir usuario
+ */
+	public function definirUsuario($usuario){
+		$this->usuario = (int)$usuario;
+	}
+
+/**
+ * Obtener usuario
+ */
+    public function obtenerUsuario()
+    {
+        if (is_null($this->usuario)) {
+            throw new Exception("El identificador del usuario no está definido.");
+        } else {
+            return $this->usuario;
+        }
+    }
+
+
+/**
+ * Crear tipo de tarifa para el registro
+ */
+    public function crearAnuncio($anuncio)
+    {
+        if ($anuncio instanceof Anuncio) {
+            $this->anuncio = $anuncio->obtenerId();
+        } else {
+            throw new Exception("El parámetro facilitado no es una instancia de la clase Anuncio");
+        }
+        return $this;
+    } 
+
+/**
+ * Método para definir el tipo de tarifa
+ */
+	public function definirAnuncio(){
+		$this->anuncio = (int)$anuncio;
+	}
+
+/**
+ * Método para obtner el tipo de tarifa
+ */
+    public function obtenerAnuncio()
+    {
+        if (is_null($this->anuncio)) {
+            throw new Exception("El anuncio no está definido");
+        } else {
+            return $this->anuncio;
+        }
+    }
+
+/**
+ * Método para definir el nombre
+ */
+ 	public function definirNombre($nombre)
+ 	{
+ 		$nombreValido = filter_var($nombre, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		if (empty($nombreValido) || strlen($nombreValido) > 64) {
+			Sesion::definirError("Campo vacío o el nombre tiene mas de 64 caracteres", "nombreServicio");
+		
+		} 	else {
+			$this->nombre = $nombreValido;
+            Sesion::definirFormulario("nombreServicio", $nombre);
+		}
+        return $this;
+ 	}
+
+/**
+ * Método para obtener el nombre
+ */
+ 	public function obtenerNombre(){
+		if (is_null($this->nombre)){
+		throw new Exception("El nombre del servicio no esta definido");
+		
+		} 	else {
+			return $this->nombre;
+		}
+	}
+
+/**
+ * Método para obtener el nombre
+ */
+public function definirDescripcion($descripcion){
+    if (empty($descripcion) || strlen($descripcion) > 512){
+        Sesion::definirError("No se ha introducido una descripción o tiene mas de 512 caracteres.", "descripcionServicio");
+    } else {
+        $this->descripcion = filter_var($descripcion, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        Sesion::definirFormulario("descripcionServicio", $descripcion);
+    }
+    return $this;
+}
+
+/**
+ * Método para obtener la descripción
+ */
+
+public function obtenerDescripcion(){   
+    if (is_null($descripcion)){
+        throw new Exception("La descripción no esta definida.");
+    } else {
+        return $this->descripcion;
+    }
+}
+/**
+ * Método para definir el precio
+ */
+	 	public function definirPrecio($precio)
+ 	{
+ 		$precioValido = filter_var($precio, FILTER_SANITIZE_NUMBER_FLOAT);
+		if ($nombreValido === false) {
+			throw new Exception("El precio introducido no es valido");
+		
+		} 	else {
+			$this->precio = $precioValido;
+		}
+ 	}
+
+/**
+ * Método para obtener el precio
+ */
+ 	public function obtenerPrecio(){
+		if (is_null($this->precio)){
+		throw new Exception("El precio no esta definido");
+		
+		} 	else {
+			return $this->precio;
+		}
+	}
+/**
+ * Método para insertar el precio
+ */
+	    public function insertar()
+    {
+        $consulta = $this->conexion->prepare("INSERT INTO `servicios` (`id_anuncio`,`id_usuarios`, `nombre`,`descripcion`, `precio` ) VALUES (?, ?, ?, ?, ?)");
+        $consulta->bind_param("iissf", 
+            $this->anuncio, 
+            $this->usuario, 
+            $this->nombre, 
+            $this->descripcion, 
+            $this->precio);
+        $result = $consulta->execute();
+        $consulta->close();
+        return $result;
+    }
+
+/**
+ * Método para leer las servicios por usuario
+ */
+
+	public function listarPorUsuario($usuario){
+
+		$servicios = [];
+        $consulta = $this->conexion->prepare("SELECT * FROM servicios WHERE id_usuarios =?");
+        $consulta->bind_param("i", $usuario);
+        $consulta->execute();
+        $resultado = $consulta->get_result();
+
+        while ($fila = $resultado->fetch_assoc()){
+
+        	$servicio = new Servicio();
+        	$servicio->definirId($fila["id"]);
+        	$servicio->definirUsuario($fila["id_usuarios"]);
+        	$servicio->definirserAnuncio($fila["id_anuncios"]);
+        	$servicio->definirNombre($fila["nombre"]);
+            $servicio->definirDescripcion($fila["descripcion"]);
+        	$servicio->definirPrecio($fila["precio"]);
+        	array_push($servicios, $servicio);
+        }
+        return $servicios;
+	}
+
+/**
+ * Método para actualizar tarifas por id
+ */
+	public function actualizar(){
+
+        $consulta = $this->conexion->prepare("UPDATE tarifas SET id_usuarios = ?, id_anuncios =?, nombre = ?, descripcion = ?, precio =? WHERE id = ?");
+        $consulta->bind_param("iissfi", 
+            $this->usuario, 
+            $this->anuncio, 
+            $this->nombre, 
+            $this->descripcion;
+            $this->precio, 
+            $this->id);
+        $resultado = $consulta->execute();
+        $consulta->close();
+        return $resultado;
+    }
+
+/**
+ * Método para eliminar tarifa por id
+ */
+    public function eliminar(){
+
+         $consulta = $this->conexion->prepare("DELETE FROM `servicios` WHERE `id` = ?");
+        $consulta->bind_param("i", $this->id);
+        $resultado = $consulta->execute();
+        $consulta->close();
+        return $resultado;
+    }
+ }
