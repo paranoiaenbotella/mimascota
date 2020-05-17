538c501b9485c7422026972bf7f44a457ac900a3
paranoiaenbotella <mikahe@tutanota.de>
2020-05-17 14:52:48 +0200

    Creating the controller file for Anuncios

---
+++ src/Controladores/Anuncios.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once(dirname(__DIR__) . "/Modelos/Anuncio.php");
+require_once(dirname(__DIR__) . "/Modelos/Usuario.php");
+require_once(dirname(__DIR__) . "/Controlador.php");
+
+/**
+ * Mediante esta clase se controla las operaciones sobre la tabla 'anuncios'
+ */
+class Anuncios extends Controlador
+{
+
+/**
+ * Método que devuelve la vista
+ */
+   protected function obtenerDirectorioVistas()
+    {
+        return dirname(__DIR__) . "/Vistas/Anuncios";
+    }
+
+/**
+  * Método que muestra el formulario de crear un anuncio
+  */
+
+public function getCrear(){
+
+}
+
+/**
+   * Mediante este método se controla la inserción del anuncio en la bd
+   */
+    public function postCrear()
+    {
+    }
+
+/**
+ * Mediante este método se muestra por pantalla el
+ * formulario para editar el anuncio
+ */
+     public function getEditar()
+    {
+        
+
+    }
+
+/**
+ * Mediante este método se controla la eliminación
+ * del anuncio
+ */
+
+public function postEliminar(){
+
+	
+}
+
+}
