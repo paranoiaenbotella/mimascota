48d3e78c8cde9f1d8df5782f367025995d175bc1
paranoiaenbotella <mikahe@tutanota.de>
2020-05-20 02:00:55 +0200

    Adding URI for usuarios and anuncios, trying to restrict the access to the Roles

--- public/index.php
+++ public/index.php
@@ -29,6 +29,26 @@ switch (1) {
             }
             $fichero = dirname(__DIR__) . "/src/Controladores/Perfil.php";
             $clase = "Perfil";
+           
+        }
+    break;
+   
+    /**
+     * Usuarios
+     */
+    case preg_match("`^/usuarios$`", $_SERVER["REQUEST_URI"]):
+        $fichero = dirname(__DIR__) . "/src/Controladores/Usuarios.php";
+        $clase = "Usuarios";
+        $accion = "getListar";
+        break;
+    case preg_match("`^/usuarios/editar/(?<id>\d+)$`", $_SERVER["REQUEST_URI"], $matches):
+        $fichero = dirname(__DIR__) . "/src/Controladores/Usuarios.php";
+        $clase = "Usuarios";
+        $argumento = $matches["id"];
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $accion = "postEditar";
+        } else {
+            $accion = "getEditar";
         }
         break;
     /**
@@ -58,23 +78,31 @@ switch (1) {
     /**
      * Roles crear
      */
-    case preg_match("`^/roles/crear$`", $_SERVER["REQUEST_URI"]);
-        $fichero = dirname(__DIR__) . "/src/Controladores/Roles.php";
-        $clase = "Roles";
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    case preg_match("`^/roles/crear$`", $_SERVER["REQUEST_URI"]):
+       if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $accion = "postCrear";
         } else {
             $accion = "getCrear";
-        }
+        } 
+        $fichero = dirname(__DIR__) . "/src/Controladores/Roles.php";
+        $clase = "Roles";
         break;
     /**
      * Roles ver
      */
     case preg_match("`^/roles$`", $_SERVER["REQUEST_URI"]):
-
+         if (!(Sesion::esAdministrador())) {
+                header("Location: /");
+            } else {
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $accion = "postCrear";
+                } else {
+                $accion = "getCrear";
+        } 
         $fichero = dirname(__DIR__) . "/src/Controladores/Roles.php";
         $clase = "Roles";
         $accion = "getListar";
+    }
         break;
     /**
      * Roles editar
@@ -297,12 +325,35 @@ switch (1) {
     /**
      * Crear anuncios
      */    
-    case preg_match("`/anuncios$/crear$`", $_SERVER["REQUEST_URI"]):
+    case preg_match("`/anuncios/crear$`", $_SERVER["REQUEST_URI"]):
+    if (Sesion::esInvitado()) {
+            header("Location: /identificacion", 301);
+        } else { 
+            $fichero = dirname(__DIR__) . "/src/Controladores/Anuncios.php";
+            $clase = "Anuncios";
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                $accion = "postCrear";
+            } else {
+                $accion = "getCrear";
+            }
+        }
     break;
     /**
      * Editar anuncios
      */
-    case preg_match("`/anuncios$/editar(?<id>\d+)$`", $_SERVER["REQUEST_URI"], $matches):
+    case preg_match("`/anuncios/editar(?<id>\d+)$`", $_SERVER["REQUEST_URI"], $matches):
+    if (Sesion::esInvitado()) {
+            header("Location: /identificacion", 301);
+        } else { 
+            $fichero = dirname(__DIR__) . "/src/Controladores/Anuncios.php";
+            $clase = "Anuncios";
+            $argumento = $matches["id"];
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                $accion = "postEditar";
+            } else {
+                $accion = "getEditar";
+            }
+        }
     break;
     default:
         echo("Error 404 - Página no encontrada");
