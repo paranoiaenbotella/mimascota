d6205a249a046a7e911f4119472dde909faac36a
paranoiaenbotella <mikahe@tutanota.de>
2020-05-23 01:30:28 +0200

    Adding methods

--- src/Controladores/Servicios.php
+++ src/Controladores/Servicios.php
@@ -16,14 +16,25 @@ class Servicios extends Controlador
     {
         return dirname(__DIR__) . "/Vistas/Servicios";
     }
+  /**
+    * Método que muestra el formulario
+    * para crear servicios
+    */  
+    public function getListar(){
+        $servicio = new Servicio();
+        $servicios = $servicio->listarPorUsuario(Sesion::obtenerUsuario()->obtenerId());
+        $usuario = new Usuario();
+        $usuario = $usuario->listarPorId(Sesion::obtenerUsuario()->obtenerId());
 
+        $this->renderizar("Listar.php", ["servicios"=>$servicios, "usuario"=>$usuario]);
+    }
     /**
      * Método que muestra el formulario
      * para crear servicios
      */
     public function getCrear()
     {
-        $servicio = new Tarifa();
+        $servicio = new Servicio();
         $servicios = $servicio->listarPorUsuario(Sesion::obtenerUsuario()->obtenerId());
         $this->renderizar("Crear.php", ["servicios" => $servicios]);
     }
@@ -32,8 +43,14 @@ class Servicios extends Controlador
      * Método para mostrar el formulario de edición
      * de los servicios
      */
-    public function getEditar()
-    {
+    public function getEditar($id)
+    {   
+        $usuario = Sesion::obtenerUsuario()->obtenerId();
+        $usuario->listarPorId($id);
+        $servicio = new Servicio();
+        $servicio = $servicio->listarPorId($id);
+        
+        $this->renderizar("Editar.php", ["servicio" => $servicio, "usuario"=>$usuario]);
     }
 
     /**
@@ -42,13 +59,13 @@ class Servicios extends Controlador
      */
     public function postCrear()
     {
-        $servicio = new Tarifa();
+        $servicio = new Servicio();
         $servicio->definirNombre($_POST["nombre"]);
         $servicio->definirPrecio($_POST["precio"]);
         $servicio->definirDescripcion($_POST["descripcion"]);
         $servicio->crearUsuario(Sesion::obtenerUsuario());
         if ($servicio->insertar()) {
-            header("Location: /servicos");
+            header("Location: /servicios");
         } else {
             header("Location: /servicios/crear");
         }
@@ -58,14 +75,36 @@ class Servicios extends Controlador
      * Método para realizar la actualización
      * del servicio
      */
-    public function postEditar()
+    public function postEditar($id)
     {
+        
+        $usuario = Sesion::obtenerUsuario();
+        $servicio = new Servicio();
+        $servicio = $servicio->listarPorId($id);
+        $servicio->definirNombre($_POST["nombre"]);
+        $servicio->definirDescripcion($_POST["descripcion"]);
+        $servicio->definirPrecio($_POST["precio"]);
+        $servicio->definirUsuario($usuario->obtenerId());
+        if ($servicio->actualizar()) {
+            header("Location: /servicios");
+        } else {
+            header("Location: /servicios/editar/$id");
+        }
+
     }
 
     /**
      * Método para eliminar el servicio
      */
-    public function postEliminar()
+    public function getEliminar($id)
     {
+       if (Sesion::esCuidador()) {
+            $servicio = new Servicio();
+            $servicio = $servicio->listarPorId($id);
+            if (isset($servicio) && $servicio->obtenerId() === (int)$id) {
+                $servicio->eliminar();
+                }
+        } else {header("Location: /");}
+        header("Location: /servicios"); 
     }
 }
