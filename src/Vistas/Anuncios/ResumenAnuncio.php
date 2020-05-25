756370696c710d0d1c15e8de6c4388bbb2337c62
paranoiaenbotella <mikahe@tutanota.de>
2020-05-25 18:54:53 +0200

    Minor changing

--- src/Vistas/Anuncios/ResumenAnuncio.php
+++ src/Vistas/Anuncios/ResumenAnuncio.php
@@ -1,25 +1,24 @@
 <!-- Resumen anuncio-->
 <section class="container">
 	<div class="row">
-		<div class="col-sm-12  col-md-12 col-lg-12"> 
+		<div class="col-sm-12  col-md-12 col-lg-12">
 			<div class="card">
 				<div class="card-header">
 					
 				</div>
 				<div class="card-body">
 					<!--IMAGEN PERFIL-->
-				<div class="row">
-					<div class="col-sm-3  col-md-3 col-lg-3">
-              <div class="card" style="width: 7rem;">
-                <img alt="imagen de perfil" class="img-thumbnail" src="">
-              </div>
-            </div>
+					<div class="row">
+						<div class="col-sm-3  col-md-3 col-lg-3">
+							<div class="card" style="width: 7rem;">
+								<img alt="imagen de perfil" class="img-thumbnail" src="">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+					</div>
 				</div>
-				<div class="row">
-
 			</div>
 		</div>
 	</div>
-</div>
-</div>
 </section>
