87748b1a980afa15ec1e05d59bce829707d58a1d
Ilya Bakhlin <github@ilyabakhlin.com>
2020-05-25 19:57:32 +0200

    Removing the announces from the profile.

--- src/Controladores/Perfil.php
+++ src/Controladores/Perfil.php
@@ -5,6 +5,7 @@ require_once(dirname(__DIR__) . "/Modelos/Usuario.php");
 require_once(dirname(__DIR__) . "/Controlador.php");
 require_once(dirname(__DIR__) . "/Modelos/Servicio.php");
 require_once(dirname(__DIR__) . "/Modelos/Anuncio.php");
+
 /**
  * Mediante esta clase se controla las operaciones relacionadas con la creacion
  * y edicion del perfil del usuario facilitandose para ello la interfaz correspondiente
@@ -27,9 +28,14 @@ class Perfil extends Controlador
             $direccion = $direccion->listarDirecciones($usuario->obtenerId());
             $servicio = new Servicio();
             $servicios = $servicio->listarPorUsuario($usuario->obtenerId());
-            $anuncio = new Anuncio();
-            $anuncio = $anuncio->listarPorUsuario($usuario->obtenerId());
-            $this->renderizar("Cuidador.php", ["direccion" => $direccion, "usuario" => $usuario, "servicios"=>$servicios, "anuncio" =>$anuncio]);
+            $this->renderizar(
+                "Cuidador.php",
+                [
+                    "direccion" => $direccion,
+                    "usuario" => $usuario,
+                    "servicios" => $servicios,
+                ]
+            );
         } else {
             $this->renderizar("Propietario.php", ["usuario" => $usuario]);
         }
