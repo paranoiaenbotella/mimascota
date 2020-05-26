38e63ffeff3de7e82f6d79c8c61ed4648d03d890
Ilya Bakhlin <github@ilyabakhlin.com>
2020-05-26 22:50:28 +0200

    Fixing the links to the opinions.

--- src/Vistas/Perfil/Propietario.php
+++ src/Vistas/Perfil/Propietario.php
@@ -9,7 +9,7 @@
                             <a class="nav-link active" href="#">Perfil</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Opiniones</a>
+                            <a class="nav-link" href="/opiniones">Opiniones</a>
                         </li>
                     </ul>
                 </div>
@@ -17,8 +17,7 @@
                     <div class="row">
                         <div class="col-sm-3  col-md-3 col-lg-2">
                             <div class="card">
-                                <img alt="imagen de perfil" class="img-thumbnail" src="<?php echo($datos["usuario"]->obtenerImagen(
-                                )); ?>">
+                                <img alt="imagen de perfil" class="img-thumbnail" src="<?php echo($datos["usuario"]->obtenerImagen()); ?>">
                                 <h6 class="text-center mt-2">Avatar</h6>
                             </div>
                         </div>
@@ -29,32 +28,28 @@
                                     <div class="input-group-prepend">
                                         <span class="input-group-text" id="nombre">Nombre:</span>
                                     </div>
-                                    <input type="text" class="form-control" name="nombre" aria-label="Nombre" aria-describedby="nombre" value="<?php echo($datos["usuario"]->obtenerNombre(
-                                    )); ?>">
+                                    <input type="text" class="form-control" name="nombre" aria-label="Nombre" aria-describedby="nombre" value="<?php echo($datos["usuario"]->obtenerNombre()); ?>">
                                 </div>
                                 <!--APELLIDOS-->
                                 <div class="input-group mb-2">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text" id="apellidos">Apellidos:</span>
                                     </div>
-                                    <input type="text" class="form-control" name="apellidos" aria-label="Apellidos" aria-describedby="apellidos" value="<?php echo($datos["usuario"]->obtenerApellidos(
-                                    )); ?>">
+                                    <input type="text" class="form-control" name="apellidos" aria-label="Apellidos" aria-describedby="apellidos" value="<?php echo($datos["usuario"]->obtenerApellidos()); ?>">
                                 </div>
                                 <!--MOVIL-->
                                 <div class="input-group mb-2">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text" id="movil">Móvil:</span>
                                     </div>
-                                    <input type="text" class="form-control" name="movil" aria-label="Móvil" aria-describedby="movil" value="<?php echo($datos["usuario"]->obtenerMovil(
-                                    )); ?>">
+                                    <input type="text" class="form-control" name="movil" aria-label="Móvil" aria-describedby="movil" value="<?php echo($datos["usuario"]->obtenerMovil()); ?>">
                                 </div>
                                 <!--CORREO ELECTRONICO-->
                                 <div class="input-group mb-2">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text" id="email">Email:</span>
                                     </div>
-                                    <input type="text" class="form-control" name="email" aria-label="Email" aria-describedby="email" value="<?php echo($datos["usuario"]->obtenerEmail(
-                                    )); ?>">
+                                    <input type="text" class="form-control" name="email" aria-label="Email" aria-describedby="email" value="<?php echo($datos["usuario"]->obtenerEmail()); ?>">
                                 </div>
                                 <!--IMAGEN-->
                                 <div class="input-group mb-2">
