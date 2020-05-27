283189eae08d02beb420a5709387aa12d7d5bfbc
paranoiaenbotella <mikahe@tutanota.de>
2020-05-27 18:02:43 +0200

    Adding views for the list of announcements, last 10, opinion and profile admin

---
+++ src/Vistas/Anuncios/Inicio.php
@@ -0,0 +1,9 @@
+<!--Ver cuidadores-->
+<section class="container">
+  <div class="row">
+    <div class="col-sm-12  col-md-12 col-lg-12 mt-3">
+    	<h1> Encuentra el mejor cuidador para tu mascota libre de jaulas</h1>
+    	<!--RESUMENES DE ANUNCIOS-->
+    </div>
+  </div>
+</section>
---
+++ src/Vistas/Anuncios/Ultimos.php
@@ -0,0 +1,39 @@
+<!--Tabla con los ultimos 10 anuncios-->
+<section class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12  col-lg-12  shadow-sm p-3 mb-5 rounded">
+            <h6 class="mb-1 mt-3"> Últimos 10 anuncios publicados:</h6>
+            
+            <table class="table table-sm table-hover border border-success">
+                <thead>
+                    <tr class="bg-success text-white">
+                        <th>Id</th>
+                        <th>Usuario</th>
+                        <th>Servicio</th>
+                        <th>Descripción</th>
+                        <th>Fecha Creación</th>
+                        <th colspan="2">Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <a href="" class="btn btn-sm btn-info">Editar
+                            </a>
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-sm btn-danger">Eliminar
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>                                  
+            </table>
+        </div>
+    </div>
+</section>
