17a2356d824c5b3b704f830c415fb6b556e683e0
paranoiaenbotella <mikahe@tutanota.de>
2020-05-27 18:52:42 +0200

    Add legal warning and Contact

---
+++ src/Vistas/Informacion/Contacto.php
@@ -0,0 +1,10 @@
+<section class="container">
+  <div class="row">
+    <div class="col-sm-12  col-md-12 col-lg-12 mt-3">
+    	<div class="row">
+    <div class="col-sm-10 offset-sm-1 col-md-10 offset-md-1 col-lg-10 offset-lg-1 mt-3">
+    	  </div>
+  </div>
+    </div>
+  </div>
+</section>
