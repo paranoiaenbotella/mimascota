b5cab56b68b6fd5b4e9a479399679c4cb841a101
paranoiaenbotella <mikahe@tutanota.de>
2020-05-28 16:55:05 +0200

    Adding some info on the creating form announcement

--- src/Vistas/Anuncios/Crear.php
+++ src/Vistas/Anuncios/Crear.php
@@ -4,7 +4,8 @@
         <div class="col-sm-12  col-md-12 col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    <h4>Crear anuncio</h4>
+                    <h4 class="mb-1">Crear anuncio</h4>
+                    <p class="text-muted mb-1"> Para poder crear un anuncio se necesita haber creado previamente los servicios oportunos.</p>
                 </div>
                 <div class="card-body">
                     <form class="row" enctype="multipart/form-data" method="post">
@@ -13,7 +14,7 @@
                                 <div class="col-lg-12">
                                     <div class="form-group my-0">
                                         <label for="nombre" class="mb-0">Nombre del Anuncio:</label>
-                                        <input class="form-control form-control-sm" id="nombre" name="nombre" type="text">
+                                        <input class="form-control form-control-sm" id="nombre" name="nombre" type="text" placeholder="Ej. Cuidador en Zona Sur de Barcelona">
                                         <?php if (Sesion::existeError(
                                             "descripcionAnuncio"
                                         )): // IMPROVE: Change the error detection. ?>
@@ -31,7 +32,7 @@
                                     <!--DESCRIPCIÓN CUIDADOR-->
                                     <div class="form-group my-0">
                                         <label for="descripcion" class="mb-0">Sobre el cuidador y alojamiento:</label>
-                                        <textarea class="form-control form-control-sm" id="descripcion" name="descripcion" rows="4"></textarea>
+                                        <textarea class="form-control form-control-sm" id="descripcion" name="descripcion" rows="4" placeholder="Descripción del cuidador y el entorno donde alojara las mascotas si es el caso."></textarea>
                                         <?php if (Sesion::existeError("descripcionAnuncio")): ?>
                                             <div>
                                                 <p class="error"><?php echo(Sesion::obtenerError(
@@ -68,6 +69,7 @@
                                     </div>
                                 </div>
                             </div>
+                            <p class="text-muted"><small> Para poder seleccionar varios valores pulse CTRL + click izquierdo en la opción deseada.</small></p>
                         </div>
                         <div class="col-lg-6">
                             <div class="row">
@@ -75,25 +77,27 @@
                                     <!--PRIMERA IMAGEN CAROUSEL-->
                                     <div class="form-group mb-2">
                                         <label for="imagen1" class="mb-0"> Imagen1:</label>
-                                        <input type="file" class="form-control" name="imagen1">
+                                        <input type="file" class="form-control form-control-sm" name="imagen1">
                                     </div>
                                     <!--SEGUNDA IMAGEN CAROUSEL-->
                                     <div class="form-group mb-2">
                                         <label for="imagen2" class="mb-0"> Imagen 2:</label>
-                                        <input type="file" class="form-control" name="imagen2">
+                                        <input type="file" class="form-control form-control-sm " name="imagen2">
                                     </div>
                                     <!--TERCERA IMAGEN CAROUSEL-->
                                     <div class="form-group mb-2">
                                         <label for="imagen3" class="mb-0"> Imagen 3:</label>
-                                        <input type="file" class="form-control" name="imagen3">
+                                        <input type="file" class="form-control form-control-sm " name="imagen3">
                                     </div>
                                     <!--CUARTA IMAGEN CAROUSEL-->
                                     <div class="form-group mb-2">
                                         <label for="imagen4" class="mb-0"> Imagen 4:</label>
-                                        <input type="file" class="form-control" name="imagen4">
+                                        <input type="file" class="form-control form-control-sm" name="imagen4">
                                     </div>
+                                    <p class="text-muted mb-1"><small> Si quiere mostrar el espacio de alojamiento suba imagenes.</small></p>
+                                    <p class="text-muted mb-1"><small> Los formatos admitidos son png y jpeg.</small></p>
                                     <div class="input-group my-2">
-                                        <button class="btn btn-secondary" type="submit">Guardar</button>
+                                        <button class="btn btn-success" type="submit">Guardar</button>
                                     </div>
                                 </div>
                             </div>
