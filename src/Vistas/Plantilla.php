29d455a9d7329ea7a161c4c53739fed55720a120
Ilya Bakhlin <github@ilyabakhlin.com>
2020-05-29 18:35:25 +0200

    Trying to fix the footer.

--- src/Vistas/Plantilla.php
+++ src/Vistas/Plantilla.php
@@ -90,23 +90,19 @@
                 </div>
             </div>
         </nav>
-        
-            <footer class="navbar navbar-expand-lg fixed-bottom">
-                <div class="container">
-                <ul class="navbar-nav">     
-                    <li class="nav-item">
-                        <a class="nav-link" href = "/aviso-legal">Aviso Legal</a>
-                    </li>                  
-                    <li class="nav-item">
-                        <a class="nav-link" href = "/contacto">Contacto</a>
-                    </li>
-                 </ul>   
-                    
-                </div>
-            </footer>
-        
-        
         <?php require_once(sprintf("%s/%s", $this->obtenerDirectorioVistas(), $vista)); ?>
+        <footer class="navbar navbar-expand-lg">
+            <div class="container">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/aviso-legal">Aviso Legal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contacto">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
         <script crossorigin="anonymous" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
         <script crossorigin="anonymous" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
         <script crossorigin="anonymous" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
