51adb58afbff3487bf5e0cce559932c6a8e80256
paranoiaenbotella <mikahe@tutanota.de>
2020-05-30 04:00:10 +0200

    The title modified and fixing the footer

--- src/Vistas/Inicio/Aviso.php
+++ src/Vistas/Inicio/Aviso.php
@@ -3,23 +3,23 @@
   <div class="row">
     <div class="col-sm-12  col-md-12 col-lg-12 mt-3">
     	<div class="row">
-    <div class="col-sm-10 offset-sm-1 col-md-10 offset-md-1 col-lg-10 offset-lg-1 mt-3">
-    <h3> Condiciones de uso</h3>	
-    <p>Usted utiliza el sitio Web en la condición de Usuario lo que implica
+    <div class="col-sm-10 offset-sm-1 col-md-10 offset-md-1 col-lg-10 offset-lg-1 mt-3 mb-1">
+    <h4> Condiciones de uso</h4>	
+    <p class="text-justify">Usted utiliza el sitio Web en la condición de Usuario lo que implica
     la aceptación completa de las condiciones y cláusulas incluidos en este documento.</p>
-    <p>Tiene la obligación de no utilizar el contenido del sitio Web de una forma que pueda dañar, inutilizar o impedir la normal utilización de los contenidos y equipos informáticos del propietario del sitio Web o de cualquier usuario de Internet.</p>
-    <h4> Opiniones</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie. Nullam finibus, justo quis semper gravida, lorem ligula mattis est, eu sagittis quam diam vel odio. Integer sed ante sed mi elementum porttitor.
+    <p class="text-justify">Tiene la obligación de no utilizar el contenido del sitio Web de una forma que pueda dañar, inutilizar o impedir la normal utilización de los contenidos y equipos informáticos del propietario del sitio Web o de cualquier usuario de Internet.</p>
+    <h6> Opiniones</h6>
+    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie. Nullam finibus, justo quis semper gravida, lorem ligula mattis est, eu sagittis quam diam vel odio. Integer sed ante sed mi elementum porttitor.
     </p>
-    <h4>Medidas de seguridad</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie. Nullam finibus, justo quis semper gravida, lorem ligula mattis est, eu sagittis quam diam vel odio. Integer sed ante sed mi elementum porttitor.
+    <h6>Medidas de seguridad</h6>
+    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie. Nullam finibus, justo quis semper gravida, lorem ligula mattis est, eu sagittis quam diam vel odio. Integer sed ante sed mi elementum porttitor.
     </p>
-    <h4>Contenidos</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie. Nullam finibus, justo quis semper gravida, lorem ligula mattis est, eu sagittis quam diam vel odio. Integer sed ante sed mi elementum porttitor.</p>
-		<h4>Propiedad intelectual e industrial</h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie. Nullam finibus, justo quis semper gravida, lorem ligula mattis est, eu sagittis quam diam vel odio. Integer sed ante sed mi elementum porttitor.</p>
-		<h4>Limitación de responsabilidad</h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie. Nullam finibus, justo quis semper gravida, lorem ligula mattis est, eu sagittis quam diam vel odio. Integer sed ante sed mi elementum porttitor.</p>
+    <h6>Contenidos</h6>
+    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie. .</p>
+	<h6>Propiedad intelectual e industrial</h6>
+	<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie.</p>
+	<h6>Limitación de responsabilidad</h6>
+	<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae turpis placerat, molestie justo et, porta dui. Donec quam ante, blandit et ipsum faucibus, commodo tempus risus. Maecenas eu orci tellus. Integer porta semper massa sit amet sagittis. Duis mollis leo id porta molestie. Nullam finibus, justo quis semper gravida, lorem ligula mattis est, eu sagittis quam diam vel odio. Integer sed ante sed mi elementum porttitor.</p>
 </div>
 </div>
 </div>
--- src/Vistas/Inicio/Inicio.php
+++ src/Vistas/Inicio/Inicio.php
@@ -8,55 +8,50 @@
         <title>MiMascota</title>
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-success">
+        <nav class="navbar navbar-expand-lg ">
             <div class="container">
                 <a class="navbar-brand" href="/">
                     <img alt="/Logo.svg" src="/Logo.svg" width="180" >
                 </a>
-                <span class="navbar-text text-white">Si te gustan los animales hazte cuidador</span>
                 <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarSupportedContent" data-toggle="collapse" type="button">
-                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"><small>Menu</small></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto mt-5">
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="/ver-anuncios">Ver Anuncios</a>
+                            <a class="nav-link" href="/ver-anuncios">Ver Anuncios</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="/registro">Registro</a>
+                            <a class="nav-link" href="/registro">Registro</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="/identificacion">Identificaci&oacute;n</a>
+                            <a class="nav-link" href="/identificacion">Identificaci&oacute;n</a>
                         </li>
                     </ul>
                 </div>
-               </div>     
+            </div>
         </nav>
-
         <div class="container">
-        <div class="row">
-            <div class="col-sm-12 col-sm-12 col-sm-12">
-       <div id="bg">
-           <img src="/fondo2.png" class="fondo">
-       </div>
-       </div>
-       </div>
-       </div>
-    <footer class="navbar navbar-expand-lg fixed-bottom">
-                <div class="container">
-                <ul class="navbar-nav">     
-                    <li class="nav-item">
-                        <a class="nav-link" href = "/aviso-legal">Aviso Legal</a>
-                    </li>                  
-                    <li class="nav-item">
-                        <a class="nav-link" href = "/contacto">Contacto</a>
-                    </li>
-                 </ul>   
-                    
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <div id="bg">
+                        <img src="/inicio.png" class="fondo">
+                    </div>
                 </div>
-            </footer>
-
-     <script crossorigin="anonymous" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+            </div>
+        </div>
+        <footer>
+            <ul class="nav justify-content-center">
+                
+                <li class="nav-item">
+                    <a class="nav-link" href = "/aviso-legal">Aviso Legal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href = "/contacto">Contacto</a>
+                </li>
+            </ul>  
+        </footer>
+        <script crossorigin="anonymous" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
         <script crossorigin="anonymous" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
         <script crossorigin="anonymous" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     </body>
