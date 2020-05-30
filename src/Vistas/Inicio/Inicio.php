8fcaec155c7921b010d8213a9e58b25f9cdfe14f
paranoiaenbotella <mikahe@tutanota.de>
2020-05-30 03:54:56 +0200

    Fixing the landing page

--- src/Vistas/Inicio/Inicio.php
+++ src/Vistas/Inicio/Inicio.php
@@ -3,23 +3,61 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" rel="stylesheet">
         <link rel="stylesheet" href="/css/inicio.css">
         <title>MiMascota</title>
     </head>
     <body>
-        <nav>
-            <div class="logo">
-            </div>
-            <div class="enlaces">
-                <a href="/ver-anuncios">Ver Anuncios</a>
-                <a href="/registro">Registrarse</a>
-                <a href="/identificacion">Iniciar Sesi&oacute;n</a>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-success">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    <img alt="/Logo.svg" src="/Logo.svg" width="180" >
+                </a>
+                <span class="navbar-text text-white">Si te gustan los animales hazte cuidador</span>
+                <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarSupportedContent" data-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="/ver-anuncios">Ver Anuncios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="/registro">Registro</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="/identificacion">Identificaci&oacute;n</a>
+                        </li>
+                    </ul>
+                </div>
+               </div>     
         </nav>
-        <section></section>
-        <footer>
-            <a href="/aviso-legal">Aviso Legal</a>
-            <a href="/sobre-nosotros">Sobre Nosotros</a>
-        </footer>
+
+        <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-sm-12 col-sm-12">
+       <div id="bg">
+           <img src="/fondo2.png" class="fondo">
+       </div>
+       </div>
+       </div>
+       </div>
+    <footer class="navbar navbar-expand-lg fixed-bottom">
+                <div class="container">
+                <ul class="navbar-nav">     
+                    <li class="nav-item">
+                        <a class="nav-link" href = "/aviso-legal">Aviso Legal</a>
+                    </li>                  
+                    <li class="nav-item">
+                        <a class="nav-link" href = "/contacto">Contacto</a>
+                    </li>
+                 </ul>   
+                    
+                </div>
+            </footer>
+
+     <script crossorigin="anonymous" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+        <script crossorigin="anonymous" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+        <script crossorigin="anonymous" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     </body>
 </html>
