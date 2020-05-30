6f127650d588bede47a0eded6bb289c869b0d35c
paranoiaenbotella <mikahe@tutanota.de>
2020-05-30 04:01:04 +0200

    Adding img and the contact page

---
+++ src/Vistas/Inicio/Contacto.php
@@ -0,0 +1,19 @@
+<!--INFORMACIÓN DE CONTACTO-->
+<section class="container">
+  <div class="row">
+    <div class="col-sm-12  col-md-12 col-lg-12 mt-3">
+    	<div class="row">
+    <div class="col-sm-10 offset-sm-1 col-md-10 offset-md-1 col-lg-10 offset-lg-1 mt-3 mb-1 p-2 bg-light">
+    <h6>Contacta con nosotros</h6>
+    <p class=" text-justify">
+		Para preguntarnos sobre alguna funcionalidad o informarnos acerca de algún error que pueda afectar de forma negativa su experiencia en nuestro sitio Web llámenos o mande un correo electrónico.
+    	</p>
+    <p class="mb-0"><b>Teléf.</b> - <i> 000 000 000</i> en 
+    						Horario: 9:00 - 17:00<p>
+    <p class="mb-3"><b>Email:</b> - <i> mimascota@mimascota.com</i> </p>
+		<p>¡Gracias por utilizar nuestra Web!</p>
+    </div>
+</div>
+</div>
+</div>
+</section>
